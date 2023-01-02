6709df5660a83b509fdc146eaa7a087bd41a5ed7
mayur.koli <mayur.koli@bookmyshow.com>
2023-01-02 12:04:13 +0530

    Initial Commit

---
+++ DC Superheroes - Final Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DC Superheroes - Final Assignment
+//
+//  Created by Mayur Koli on 02/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
